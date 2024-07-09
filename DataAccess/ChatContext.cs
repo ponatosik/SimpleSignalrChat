d42922682959def1e7d00695dd6eb958d19ee5e2
ponatosik <ponatosik@gmail.com>
2024-07-09 20:58:27 +0300

    feat: add entity framework db context

---
+++ DataAccess/ChatContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleSignalrChat.DataAccess.Entities;
+
+namespace SimpleSignalrChat.DataAccess;
+
+public class ChatContext : DbContext
+{
+	private DbSet<Chat> Chats { get; set; }
+	private DbSet<User> Users { get; set; }
+	private DbSet<Message> Messages { get; set; }
+}
