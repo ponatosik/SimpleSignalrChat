8ecf6c2f16f9c775d1e3faac68f36c549304742b
ponatosik <ponatosik@gmail.com>
2024-07-09 20:26:11 +0300

    feat: add entities

---
+++ DataAccess/Entities/Chat.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleSignalrChat.DataAccess.Entities;
+
+public class Chat
+{
+	public int Id { get; set; }
+	public string Name { get; set; }
+	public User Admin { get; set; }
+	public ICollection<Message> Messages { get; set; }
+}
---
+++ DataAccess/Entities/Message.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleSignalrChat.DataAccess.Entities;
+
+public class Message
+{
+	public int Id { get; set; }
+	public string Content { get; set; }
+	public DateTime SentAt { get; set; }
+	public User Sender { get; set; }
+	public Chat Chat { get; set; }
+}
