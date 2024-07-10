0d2a80bf4ac55b3084dc3bb0e5e4cee970b14399
ponatosik <ponatosik@gmail.com>
2024-07-10 10:15:08 +0300

    refactor: move project to separate folder

--- src/DataAccess/ChatContext.cs
+++ src/DataAccess/ChatContext.cs
@@ -8,4 +8,6 @@ public class ChatContext : DbContext
 	public DbSet<Chat> Chats { get; set; }
 	public DbSet<User> Users { get; set; }
 	public DbSet<Message> Messages { get; set; }
+
+    public ChatContext(DbContextOptions<ChatContext> options) : base(options) { }
 }
